2c49b995c621bb77af839044e69f83dad0e6197a
Boonyakorn48 <124240744+Boonyakorn48@users.noreply.github.com>
2026-03-02 17:06:26 +0700

    Add files via upload

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,28 +1,40 @@
-function summiData(){
-    let fristNameDom = document.querySelector('input[name=fristName]');
-    let lastNameDom = document.querySelector('input[name=lastname]');
-    let ageDom = document.querySelector('input[name=age]');
+const submitData  = async () => {
+    let firstNameDOM = document.querySelector('input[name=firstname]');
+    let lastNameDOM = document.querySelector('input[name=lastname]');
+    let ageDOM = document.querySelector('input[name=age]');
     let genderDOM = document.querySelector('input[name=gender]:checked');
-    let interestsDOMs = document.querySelectorAll('input[name=interests]:checked')
+    let interestDOMs = document.querySelectorAll('input[name=interests]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
+    let messageDOM = document.getElementById('message');
+    try {
+        let interest = ''
+        for (let i = 0; i < interestDOMs.length; i++) {
+            interest += interestDOMs[i].value 
+            if (i != interestDOMs.length - 1) {
+             interest += ','
+            }
+         }
 
-    let interest = ''
-    for (let i = 0; i < interestsDOMs.length; i++){
-        interest += interestsDOMs[i].value
-        if (i != interestsDOMs.length - 1){
-            interest += ','
+        let userData ={
+            firstName: firstNameDOM.value,
+            lastName: lastNameDOM.value,
+            age: ageDOM.value,
+            gender: genderDOM.value,
+            description: descriptionDOM.value,
+            interests: interest
         }
-    }
-
-    let userData = {
-        fristName: fristNameDom.value,
-        lastName: lastNameDom.value,
-        age: ageDom.value,
-        gender: genderDOM.value,
-        descriptionDOM: descriptionDOM.value,
-        interest: interest
-
-    }
-    console.log('submit data',userData);
+    
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('response', response.data);
+        messageDOM.innerText = 'บันทึกข้อมูลสำเร็จ'
+        messageDOM.className = 'message succes'
+    } catch (error) {
+        if (error.response) {
+            console.log('Error response', error.response.data.message);
+        }
+        messageDOM.innerText = 'การบันทึกข้อมูลผิดพลาด: '
+        messageDOM.className = 'message danger'
+    }    
+     console.log('submitData', userData);
 }
